eaf926cfc0f1c70d6e5e16f630348e5153a0855c
André Velloso <andrevvelloso@gmail.com>
2021-02-02 18:32:47 +0000

    Encapsula variaveis de ambiente com dotenv

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+/* 
+API de CRUD - Persistencia no MongoDB 
+Poderá utilizar esta ferramenta online para testar API em  https://app.boomerangapi.com/
+by André Velloso  
+*/
+
 import express from 'express';
 import mongoose from 'mongoose';
 import {studentRouter} from './routes/studentRouter.js';
 import {professorRouter} from './routes/professorRouter.js';
-
-// API de CRUD - Persistencia no MongoDB 
-// Ferramenta online para testar API em  https://app.boomerangapi.com/
+import pkg from 'dotenv';
+const {dotenv} = pkg;
 
 // Conectando ao DB igti na Cloud do Atlas
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://admin:XCsL3xruyw7H7H1e@cluster0.ltypa.mongodb.net/igti?retryWrites=true&w=majority', {
+    await mongoose.connect('mongodb+srv://'+process.env.DB_USER+':'+process.env.DB_PASS+'@cluster0.ltypa.mongodb.net/igti?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false,
@@ -18,12 +23,9 @@ import {professorRouter} from './routes/professorRouter.js';
   } catch (error) {console.log("Erro ao conectar ao MongoDB Atlas "+error);}
 })();
 
-// Alteracao para testar o git
-
 const app = express();
 
 app.use(express.json());
 app.use(studentRouter);
 app.use(professorRouter);
-
-app.listen(3001, ()=> console.log('API Iniciada na porta 3001'));
+app.listen(process.env.PORT, ()=> console.log('API Iniciada na porta '+process.env.PORT));
